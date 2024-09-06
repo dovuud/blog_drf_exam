5d6e6c612e9fd8450a6829568d0f8ac776123260
Lenovo <tipmeofficial.22@gmail.com>
2024-09-06 18:41:14 +0500

    first commit

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('post/',PostList.as_view(),name='post-list'),
     path('post/<int:pk>',PostDetail.as_view(),name='post-detail'),
     path('category/',CategoryList.as_view(),name='category-list'),
+
 ]
